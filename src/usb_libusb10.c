40e9ca43a2674dc52fcbac223de7506939302e9b
Piyush Khandelwal <piyushk@gmail.com>
2012-11-06 16:44:08 -0600

    temp

--- src/usb_libusb10.c
+++ src/usb_libusb10.c
@@ -570,7 +570,8 @@ FN_INTERNAL int fnusb_stop_iso(fnusb_dev *dev, fnusb_isoc_stream *strm)
 
 FN_INTERNAL int fnusb_control(fnusb_dev *dev, uint8_t bmRequestType, uint8_t bRequest, uint16_t wValue, uint16_t wIndex, uint8_t *data, uint16_t wLength)
 {
-  int length = libusb_control_transfer(dev->dev, bmRequestType, bRequest, wValue, wIndex, data, wLength, 2);
+  int ret;
+  int length = libusb_interrupt_transfer(dev->dev, bmRequestType, data, wLength, &ret, 0);
   if (bmRequestType == 0xc0) {
     printf("Length of returned packet: %d, expected length: %d\n", length, wLength);
     int i = 0;
